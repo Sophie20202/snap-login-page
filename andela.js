54581fd2a061729247d162961da8c2fd69161237
Sophie20202 <sofidele12@gmail.com>
2024-01-02 06:58:32 -0800

    first commit

---
+++ andela.js
@@ -0,0 +1,10 @@
+function Vowel(str){
+    let arr=['a','i','o','u','e'];
+    let result="";
+    for(let i=0;i<str.length;i++){
+if ( !arr[i].includes) 
+result+=str[i]
+    }
+  }
+  let str='jesus is good';
+  console.log(Vowel(str));
